721b6b9ed7ad80d57d1c63aeb120e791524f1f4f
MajdFoudeh <majdfoude@gmail.com>
2023-03-27 23:54:05 +0300

    update

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
+let arr=[];
+if(localStorage.arr != null){
+    arr=JSON.parse(localStorage.arr);
+    render();
+}
 let form = document.getElementById("new-student");
 let students = document.getElementById("students-list");
+let card =document.getElementById("card");
+
+
 form.addEventListener("submit", (event) => {
     event.preventDefault();
     let full_name = event.target.fullName.value;
@@ -7,12 +15,19 @@ form.addEventListener("submit", (event) => {
     let phone_number = event.target.phoneNumber.value;
     let grade = event.target.Grade.value;
     let gender = event.target.gender.value;
-    console.log(full_name, date_of_birth, phone_number, grade, gender);
-    render(full_name, date_of_birth, phone_number, grade, gender);
+    let major = event.target.major.value;
+    let image =event.target.image.value;
+    console.log(full_name, date_of_birth, phone_number, grade, gender,major,image);
     form.reset();
+
+let std1 =new Student(full_name, date_of_birth, phone_number, grade, gender,major,image);
+arr.push(std1);
+localStorage.setItem("arr",JSON.stringify(arr));
+    render1(full_name, date_of_birth, phone_number, grade, gender);
+    render();
 });
 let n = 0;
-function render(full_name, date_of_birth, phone_number, grade, gender) {
+function render1(full_name, date_of_birth, phone_number, grade, gender) {
 
     n++;
 
@@ -38,4 +53,39 @@ function render(full_name, date_of_birth, phone_number, grade, gender) {
     tr.appendChild(td6);
     students.appendChild(tr);
 
+}
+function Student(full_name, date_of_birth, phone_number, grade, gender,major,image){
+this.full_name=full_name;
+this.date_of_birth=date_of_birth;
+this.phone_number=phone_number;
+this.grade=grade;
+this.gender=gender;
+this.major=major;
+this.image=image;
+}
+
+function render(){
+for (let i = 0; i < arr.length; i++) {
+
+    
+
+
+    let div =document.createElement("div");
+let ul =document.createElement("ul");
+let img1 =document.createElement("img");
+img1.src=`${arr[i].image}`;
+let li2 =document.createElement("li")
+li2.textContent=`Name: ${arr[i].full_name}`;
+let li3 =document.createElement("li")
+li3.textContent=`Phone Number: ${arr[i].phone_number}`;
+let li4 =document.createElement("li")
+li4.textContent=`Major: ${arr[i].major}`;
+
+div.appendChild(img1)
+ul.appendChild(li2)
+ul.appendChild(li3)
+ul.appendChild(li4)
+div.appendChild(ul)
+card.appendChild(div)
+}
 }
